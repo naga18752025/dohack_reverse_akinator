7d2b7f9096973acee2bdd3383106a5281e7a2e51
naga <doshisha.ryota.813@gmail.com>
2025-09-22 20:11:37 +0900

    微調整

--- js/game.js
+++ js/game.js
@@ -285,7 +285,9 @@ async function answerCheck(){
     document.querySelectorAll(".response").forEach(R => {
         R.style.display = "none";
     })
-    document.querySelector(".hint").style.display = "none";
+    if(document.querySelector(".hint")){
+        document.querySelector(".hint").style.display = "none";
+    }
 
     // 解答確認中の画面を作成して表示
     const newComment = document.createElement("div");
